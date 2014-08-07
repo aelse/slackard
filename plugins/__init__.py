e2b6bea8058203c702e55a25791f4763e05582ea
Alexander Else <alexander.else@team.telstra.com>
2014-08-07 11:01:49 +1000

    Python3 exception handling

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -11,5 +11,5 @@ def init_plugins(bot_):
     for plugin in glob('plugins/[!_]*.py'):
         try:
             importlib.import_module(plugin.replace('/', '.')[:-3])
-        except Exception, e:
+        except Exception as e:
             print 'Failed to import {0}: {1}'.format(plugin, e)
