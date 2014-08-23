6ffed718ff69f877032850c305fc8d29d319124b
Alexander Else <aelse@else.id.au>
2014-08-23 19:05:30 +1000

    Bump version 1.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='slackard',
     license='BSD',
     py_modules=['slackard'],
-    version='1.0.3',
+    version='1.0.4',
     install_requires=['PyYAML', 'requests', 'slacker'],
 
     description='A pluggable bot for slack.com',
