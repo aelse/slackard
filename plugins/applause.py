35a164018744694a45f4a0007000227d86c2046a
Alexander Else <aelse@else.id.au>
2015-08-09 17:45:34 +1000

    Fix sample firehose message handling

--- plugins/applause.py
+++ plugins/applause.py
@@ -4,10 +4,10 @@ import re
 @bot.firehose
 def listen(message):
     # Attempt to strip out URLs
-    message= re.sub('(https?|ftp)://\S+', '', message['text'])
+    message = re.sub('(https?|ftp)://\S+', '', message['text'])
 
     pattern = "applau(d|se)|bravo|slow clap"
-    m = re.search(pattern, message['text'], re.IGNORECASE)
+    m = re.search(pattern, message, re.IGNORECASE)
     if m:
         image = 'http://i.imgur.com/9Zv4V.gif'
         bot.speak(image)
